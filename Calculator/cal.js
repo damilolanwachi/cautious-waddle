d1a8a88e0b3254de35255fc27129ae9e70bccaec
damilolanwachi <damilolanwachi@gmail.com>
2022-08-12 12:32:28 +0100

    Initial commit

---
+++ Calculator/cal.js
@@ -0,0 +1,9 @@
+let first = document.getElementById('first').value;
+    let operator = document.getElementById('operator');
+    let second = document.getElementById('second').value;
+    
+function calculate() {
+    if(operator.value == '+') {
+        first + second;
+    }
+}
